e609f09880de0aef66373915ee6476506e04896c
Michelangelo Ungolo <ungolomiki46@gmail.com>
2024-05-19 18:19:59 +0200

    created and updated frontend to project

--- ELiteCloud/EliteDownload/views.py
+++ ELiteCloud/EliteDownload/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, 'EliteDownload/login.html')
+
+def register(request):
+    return render(request, 'EliteDownload/register.html')
+
+def download(request):
+    return render(request, 'EliteDownload/cloud.html')
